19ea720d766b18f40e23473fb1419b7d480ccf59
ZEYAD-8 <zeyadosama15@gmail.com>
2023-08-09 05:56:11 +0300

    Task 12: Fizz Buzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for num in range(1, 101):
+        if num % 3 == 0:
+            print("Fizz", end="")
+        if num % 5 == 0:
+            print("Buzz", end="")
+        print(" ", end="")
