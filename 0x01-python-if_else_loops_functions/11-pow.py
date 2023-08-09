0edfa0e1552714be3e1846ca79df929b5e31cd63
ZEYAD-8 <zeyadosama15@gmail.com>
2023-08-09 05:49:37 +0300

    Task 11: a ^ b

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -2,7 +2,7 @@
 def pow(a, b):
     if type(a) != int or type(b) != int:
         raise TypeError
-    res = 0
-    for iteration in range(b):
-        res = res + (a * a)
+    res = 1
+    for _ in range(b):
+        res *= a
     return res
