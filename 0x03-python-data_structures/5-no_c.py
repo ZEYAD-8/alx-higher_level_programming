c2a1565514e01d8b1ca9b99041263b3695b232e3
ZEYAD-8 <zeyadosama15@gmail.com>
2023-08-15 06:22:21 +0300

    Task 5: Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,18 @@
 def no_c(my_string):
     new_string = ""
     for i in my_string:
-        if i not in "cC":
+        if i not in "Cc":
             new_string += i
     return new_string
+###
+# my first solve was like this:
+    # new_string = my_string[0:]
+    # chars_emitted = i = 0
+    # for char in my_string:
+    #     i += 1
+    #     if char in "Cc":
+    #         new_string = new_string[0:i - chars_emitted - 1] + new_string[i - chars_emitted:len(new_string)]
+    #         chars_emitted += 1
+    # return new_string
+# I know i know, still thinking the C way not the python way.
+###
