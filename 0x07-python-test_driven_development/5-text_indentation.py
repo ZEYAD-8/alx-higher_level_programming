4046ca97208634317c4b3d74dfe3dc1637edee57
ZEYAD-8 <zeyadosama15@gmail.com>
2023-09-06 05:30:05 +0300

    Task 4: Text indentatio

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,10 +1,30 @@
+#!/usr/bin/python3
+"""Defines a text-indentation function."""
 
 def text_indentation(text):
+    """
+        Print a text with two new lines after each '.', '?', and ':' character.
+
+        Args:
+        text (str): The input text to be formatted and printed.
+
+        Returns:
+        None: This function doesn't return a value; it prints the formatted text directly.
+    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    
-    endings = {".", "?", ":"}
-    for letter in text:
-        print(letter, end="")
-        if letter in endings:
-            print("\n")
+
+    character = 0
+    while character < len(text) and text[character] == ' ':
+        character += 1
+
+    while character < len(text):
+        print(text[character], end="")
+        if text[character] == "\n" or text[character] in ".?:":
+            if text[character] in "?.:":
+                print("\n")
+            character += 1
+            while character < len(text) and text[character] == ' ':
+                character += 1
+            continue
+        character += 1
