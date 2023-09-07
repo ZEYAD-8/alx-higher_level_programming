e4022014caa3d778b0189207051d5fc6195eb00a
ZEYAD-8 <zeyadosama15@gmail.com>
2023-09-07 04:29:53 +0300

    Task 30: Low memory cost

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -2,7 +2,8 @@
 """
 Locked Class Module
 
-This module defines a Python class called LockedClass that demonstrates the use of __slots__
+This module defines a Python class called LockedClass
+that demonstrates the use of __slots__
 to restrict the attributes that instances of the class can have.
 
 Usage:
@@ -14,14 +15,15 @@ Usage:
     # Assign a value to the 'first_name' attribute
     obj.first_name = "John"
 
-    # Attempt to assign a value to a non-existent attribute (will raise an AttributeError)
+    # Attempt to assign a value to a non-existent attribute
+        (will raise an AttributeError)
     obj.last_name = "Doe"
 """
 
 
 class LockedClass:
-    """ 
-    Prevent the user from instantiating new LockedClass attributes 
-    for anything but attributes called 'first_name'. 
+    """
+    Prevent the user from instantiating new LockedClass attributes
+    for anything but attributes called 'first_name'.
     """
     __slots__ = ["first_name"]
