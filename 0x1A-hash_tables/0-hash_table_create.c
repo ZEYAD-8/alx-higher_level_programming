c9b9998782e73dddc72c50cd21cf9777eff7dfac
ZEYAD-8 <zeyadosama15@gmail.com>
2023-09-09 00:52:07 +0300

    Task 0: >>> ht = {}

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - Creates a hash table
+ * @size: Size of hash Table array
+ *
+ * Return: pointer to the hash table
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table = malloc(sizeof(hash_table_t));
+
+	if (table == NULL)
+	{
+		return (NULL);
+	}
+
+	hash_node_t *bucket = malloc(sizeof(hash_node_t) * size);
+	if (bucket == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
+	table->size = size;
+	table->array = &bucket;
+	return (table);
+}
