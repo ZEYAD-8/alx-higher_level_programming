53510be792909ac8b4f50af6286ed0fbb61a2cff
ZEYAD-8 <zeyadosama15@gmail.com>
2023-09-14 02:49:07 +0300

    Task 12: Pascal's Triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""Defines a Pascal's Triangle function.""" 
+"""Defines a Pascal's Triangle function."""
 
 
 def pascal_triangle(n):
     """
     Represent Pascal's Triangle of size n.
     Returns a list of lists of integers representing the triangle.
-    """ 
+    """
     pascal = []
     if n <= 0:
         return pascal
