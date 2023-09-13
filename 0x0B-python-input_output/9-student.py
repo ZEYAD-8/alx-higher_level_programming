947c4e8cf5c9514968895a5d7278d10298e38150
ZEYAD-8 <zeyadosama15@gmail.com>
2023-09-13 23:16:39 +0300

    Task 9: Student to JSON

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""module that defines the studned class"""
+
+
+class Student:
+    """
+    Represents a student with attributes such as first name, last name, and age.
+    """
+    def __init__(self, first_name, last_name, age):
+        """Initializes a new Student instance."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Retrieves a dictionary representation of the Student instance."""
+        return self.__dict__
