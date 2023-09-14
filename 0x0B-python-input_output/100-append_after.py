29a8a4ceb8d1c319db1acca7ad370bdc0fd02ed6
ZEYAD-8 <zeyadosama15@gmail.com>
2023-09-14 03:10:11 +0300

    Task 13 (advanced): Search and update

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Module that executes a function that appends a line """
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """ 
+    Function that appends a new line when a string is found
+    """
+
+    res_line = []
+    with open(filename, 'r', encoding="utf-8") as f:
+        for line in f:
+            res_line += [line]
+            if line.find(search_string) != -1:
+                res_line += [new_string]
+
+    with open(filename, 'w', encoding="utf-8") as f:
+        f.write("".join(res_line))
