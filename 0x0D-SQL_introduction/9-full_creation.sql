483f8183c10bb840a1007d72b08861d740be8775
ZEYAD-8 <zeyadosama15@gmail.com>
2023-10-18 22:10:02 +0300

    Task 9: Full creation

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,13 @@
+-- Create the second_table if it doesn't exist
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+
+-- Insert records into the second_table
+INSERT INTO second_table (id, name, score) VALUES
+    (1, 'John', 10),
+    (2, 'Alex', 3),
+    (3, 'Bob', 14),
+    (4, 'George', 8);
