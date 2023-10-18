b90bb9da7920e6ba7c952dcf6dbad9b9e68f3323
ZEYAD-8 <zeyadosama15@gmail.com>
2023-10-18 21:41:44 +0300

    Task 5: Full description

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,2 @@
 -- A script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
-SHOW COLUMNS FROM first_table;
+SHOW CREATE TABLE first_table;
