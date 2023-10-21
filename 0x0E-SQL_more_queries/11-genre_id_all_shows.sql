38fae2d78d3fedc6e61f17f1d6319b877a1c2b9c
ZEYAD-8 <zeyadosama15@gmail.com>
2023-10-21 04:38:11 +0300

    Task 11: Genre ID for all shows

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -2,6 +2,6 @@
 
 SELECT tv_shows.title, tv_show_genres.genre_id 
 FROM tv_shows 
-INNER JOIN tv_show_genres 
+LEFT JOIN tv_show_genres 
 ON tv_shows.id = tv_show_genres.show_id 
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
