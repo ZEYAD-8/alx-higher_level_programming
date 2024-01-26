ff65306586aab895c366b770826d7df77fd3e1e5
ZEYAD-8 <zeyadosama15@gmail.com>
2024-01-26 02:30:33 +0200

    Task 7: I love C

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,7 +4,7 @@ let converted = parseInt(process.argv[2]);
 if (!converted) {
   console.log('Missing number of occurrences');
 } else {
-  while (converted) {
+  while (converted > 0) {
     console.log('C is fun');
     converted--;
   }
