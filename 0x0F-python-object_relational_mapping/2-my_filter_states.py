042ed00e448179436b7fe527ea16b1944443f6d9
ZEYAD-8 <zeyadosama15@gmail.com>
2024-01-28 20:14:34 +0200

    Task 2: Filter states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,31 +7,33 @@ a given name
 import MySQLdb
 from sys import argv
 
+if __name__ == "__main__":
+    mysql_username = argv[1]
+    mysql_password = argv[2]
+    db_name = argv[3]
+    state_searched = argv[4]
 
-mysql_username = argv[1]
-mysql_password = argv[2]
-db_name = argv[3]
-state_name_searched = argv[4]
+    connection = MySQLdb.connect(
+        user=mysql_username,
+        passwd=mysql_password,
+        host='localhost',
+        database=db_name,
+        port=3306
+    )
 
-connection = MySQLdb.connect(
-    user=mysql_username,
-    passwd=mysql_password,
-    host='localhost',
-    database=db_name,
-    port=3306
-)
+    cursor = connection.cursor()
+    try:
+        cursor.execute("SELECT *\
+                       FROM states\
+                       WHERE name='{:s}'\
+                       ORDER BY id ASC".format(state_searched))
+        result = cursor.fetchall()
+        for row in result:
+            print(row)
 
-cursor = connection.cursor()
-try:
-    cursor.execute("SELECT * FROM states WHERE name='{:s}' ORDER BY id ASC"
-                   .format(state_name_searched))
-    result = cursor.fetchall()
-    for row in result:
-        print(row)
+    except MySQLdb.Error as e:
+        print(f"Error: {e}")
 
-except MySQLdb.Error as e:
-    print(f"Error: {e}")
-
-finally:
-    cursor.close()
-    connection.close()
+    finally:
+        cursor.close()
+        connection.close()
