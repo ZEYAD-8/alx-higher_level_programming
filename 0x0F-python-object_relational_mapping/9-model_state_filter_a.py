50a283b891f457681ce29435da68f255d5a66df5
ZEYAD-8 <zeyadosama15@gmail.com>
2024-01-28 20:39:08 +0200

    Task 9: Contains the letter a

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -3,6 +3,7 @@
 list all State objects that contain the letter a from a database
 """
 
+import sqlalchemy
 from model_state import Base, State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
