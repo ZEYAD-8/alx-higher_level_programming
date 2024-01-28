debd2c0614cdc4f13c2d7b21d25ffac4317166d8
ZEYAD-8 <zeyadosama15@gmail.com>
2024-01-28 20:04:04 +0200

    Task 1: Filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+python script that lists all states from the database hbtn_0e_0_usa with a name
+starting with N
+"""
+
+import MySQLdb
+from sys import argv
+
+
+mysql_username = argv[1]
+mysql_password = argv[2]
+db_name = argv[3]
+
+connection = MySQLdb.connect(
+    user=mysql_username,
+    passwd=mysql_password,
+    host='localhost',
+    database=db_name,
+    port=3306
+)
+
+cursor = connection.cursor()
+try:
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    result = cursor.fetchall()
+    for row in result:
+        print(row)
+
+except MySQLdb.Error as e:
+    print(f"Error: {e}")
+
+finally:
+    cursor.close()
+    connection.close()
