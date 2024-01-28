6a7317882c41ea71e287a7d2307d1528296a33a7
ZEYAD-8 <zeyadosama15@gmail.com>
2024-01-28 20:06:59 +0200

    Task 1: Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,29 +7,33 @@ starting with N
 import MySQLdb
 from sys import argv
 
+if __name__ == "__main__":
+    mysql_username = argv[1]
+    mysql_password = argv[2]
+    db_name = argv[3]
 
-mysql_username = argv[1]
-mysql_password = argv[2]
-db_name = argv[3]
+    connection = MySQLdb.connect(
+        user=mysql_username,
+        passwd=mysql_password,
+        host='localhost',
+        database=db_name,
+        port=3306
+    )
 
-connection = MySQLdb.connect(
-    user=mysql_username,
-    passwd=mysql_password,
-    host='localhost',
-    database=db_name,
-    port=3306
-)
+    cursor = connection.cursor()
+    try:
+        cursor.execute("SELECT * \
+                       FROM states\
+                       WHERE name LIKE 'N%'\
+                       ORDER BY id ASC")
 
-cursor = connection.cursor()
-try:
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-    result = cursor.fetchall()
-    for row in result:
-        print(row)
+        result = cursor.fetchall()
+        for row in result:
+            print(row)
 
-except MySQLdb.Error as e:
-    print(f"Error: {e}")
+    except MySQLdb.Error as e:
+        print(f"Error: {e}")
 
-finally:
-    cursor.close()
-    connection.close()
+    finally:
+        cursor.close()
+        connection.close()
