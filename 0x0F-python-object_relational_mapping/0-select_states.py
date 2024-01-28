d99eef337cdd540c0bcb24c250d90073dc3c5aa5
ZEYAD-8 <zeyadosama15@gmail.com>
2024-01-28 20:02:23 +0200

    Task 0: Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,28 +6,30 @@ python script that lists all states from the database hbtn_0e_0_usa
 import MySQLdb
 from sys import argv
 
-mysql_username = argv[1]
-mysql_password = argv[2]
-db_name = argv[3]
 
-connection = MySQLdb.connect(
-    user=mysql_username,
-    passwd=mysql_password,
-    host='localhost',
-    database=db_name,
-    port=3306
-)
+if __name__ == "__main__":
+    mysql_username = argv[1]
+    mysql_password = argv[2]
+    db_name = argv[3]
 
-cursor = connection.cursor()
-try:
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    result = cursor.fetchall()
-    for row in result:
-        print(row)
+    connection = MySQLdb.connect(
+        user=mysql_username,
+        passwd=mysql_password,
+        host='localhost',
+        database=db_name,
+        port=3306
+    )
 
-except MySQLdb.Error as e:
-    print(f"Error: {e}")
+    cursor = connection.cursor()
+    try:
+        cursor.execute("SELECT * FROM states ORDER BY id ASC")
+        result = cursor.fetchall()
+        for row in result:
+            print(row)
 
-finally:
-    cursor.close()
-    connection.close()
+    except MySQLdb.Error as e:
+        print(f"Error: {e}")
+
+    finally:
+        cursor.close()
+        connection.close()
