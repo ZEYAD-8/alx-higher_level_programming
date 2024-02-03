e4e6daab0a72125cecd4d687a8ecd1b7963c91db
ZEYAD-8 <zeyadosama15@gmail.com>
2024-02-03 23:09:01 +0200

    Task 13: Delete states

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+changes the name of the State object where id=2 to New Mexico from a database
+"""
+
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    with Session() as session:
+        result = session.query(State).filter(State.name.like(f'%a%')).all()
+        for row in result:
+            session.delete(row)
+        session.commit()
