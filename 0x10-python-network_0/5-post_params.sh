7577d1b27995be4a83036cc703106759376cf358
ZEYAD-8 <zeyadosama15@gmail.com>
2024-02-09 03:48:40 +0200

    Task 5: cURL POST parameters

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -s "$1" -X GET -H "X-School-User-Id: 98"
+# takes in a URL as an argument, sends a POST request to the passed URL, and displays the body of the response
+curl -s "$1" -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"
