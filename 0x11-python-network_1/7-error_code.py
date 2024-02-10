31f62ca21a0ec4afaadf44f15ab5573214abbe82
ZEYAD-8 <zeyadosama15@gmail.com>
2024-02-10 05:13:38 +0200

    Task 7: Error code #1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """Takes in a URL, sends a request to the URL
-and displays the value of the variable X-Request-Id
-in the response header"""
+and displays the body of the response. If the HTTP status code is greater than
+or equal to 400, will print Error code: followed by the HTTP status code"""
 
 if __name__ == '__main__':
     import sys
     import requests
-    try:
-        response = requests.get(sys.argv[1])
-        print(response.headers.get('X-Request-Id'))
-    except Exception:
-        pass
+    url = sys.argv[1]
+    response = requests.get(url)
+    if response.status_code >= 400:
+        print("Error code: {}".format(response.status_code))
+    else:
+        print(response.text)
