744515f4640e83d12128288cc15c1459e6c5c30d
ZEYAD-8 <zeyadosama15@gmail.com>
2024-02-10 04:18:14 +0200

    Task 0: What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-"""A python script that fetches https://alx-intranet.hbtn.io/status"""
+"""fetches https://alx-intranet.hbtn.io/status"""
 
 
 if __name__ == '__main__':
     import urllib.request
-    url = 'https://alx-intranet.hbtn.io/status'
-    with urllib.request.urlopen(url) as response:
-        body = response.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(body)))
-        print('\t- content: {}'.format(body))
-        print('\t- utf8 content: {}'.format(body.decode('utf-8')))
+    url = "https://alx-intranet.hbtn.io/status"
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+        print(f"Body response:")
+        print(f"\t- type: {type(html)}")
+        print(f"\t- content: {html}")
+        print(f"\t- utf8 content: {html.decode('UTF-8')}")
