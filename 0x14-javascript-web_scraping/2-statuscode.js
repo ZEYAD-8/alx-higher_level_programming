2bbbe9e098dfe82df6fb93a7acbfa2eea9e5ffec
ZEYAD-8 <zeyadosama15@gmail.com>
2024-02-25 11:49:23 -0800

    Status Code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = process.argv[2];
+
+request.get(url, (err, response) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log('code: ' + response.statusCode);
+});
