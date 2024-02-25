258067369bbee861ff016eb650e5f8287ace1924
ZEYAD-8 <zeyadosama15@gmail.com>
2024-02-26 00:42:16 +0200

    Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,12 +2,22 @@
 
 const request = require('request');
 
-const url = 'https://swapi-api.alx-tools.com/api/people/18/';
+const url = process.argv[2];
 request(url, (err, res, body) => {
   if (err) {
     console.log(err);
     return;
   }
   const data = JSON.parse(body);
-  console.log(data.films.length);
+
+  let count = 0;
+  for (let i = 0, len = data.results.length; i < len; i++) {
+    for (let z = 0, len = data.results[i].characters.length; z < len; z++) {
+      const Test = data.results[i].characters[z];
+      if (Test.includes('18')) {
+        count++;
+      }
+    }
+  }
+  console.log(count);
 });
