2501b77e1ddf30896116507bd3d9482f19130d3a
ZEYAD-8 <zeyadosama15@gmail.com>
2024-02-25 20:30:44 +0200

    Write to a file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const filePath = process.argv[2];
+const string = process.argv[3];
+
+fs.writeFile(filePath, string, 'utf8', (err) => {
+  if (err) {
+    console.error(err);
+  }
+});
