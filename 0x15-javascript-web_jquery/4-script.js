d9f51cadcc3924b735a6e2b44696070222d64688
ZEYAD-8 <zeyadosama15@gmail.com>
2024-03-04 04:15:29 +0200

    Toggles classes when clicked

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,11 @@
+// 4. Toggle classes
+
+$('DIV#toggle_header').click(function () {
+  if ($('header').hasClass('red')) {
+    $('header').removeClass('red');
+    $('header').addClass('green');
+  } else {
+    $('header').removeClass('green');
+    $('header').addClass('red');
+  }
+});
