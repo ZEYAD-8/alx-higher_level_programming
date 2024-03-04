de497bab74354cb102e93ee63635ac2e4e197a1b
ZEYAD-8 <zeyadosama15@gmail.com>
2024-03-04 04:22:32 +0200

    Change the <header> text when clicked

---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,5 @@
+// 6. Change the text of the header to "New Header!!!" when the user clicks on DIV#update_header.
+
+$('DIV#update_header').click(function () {
+  $('header').text('New Header!!!');
+});
