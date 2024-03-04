a72e6676c6de854afb8cc09e40b52e3155fc450a
ZEYAD-8 <zeyadosama15@gmail.com>
2024-03-04 05:42:44 +0200

    List, Add, Clear

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,18 @@
+// List, add, remove.
+
+$(document).ready(function () {
+  $('DIV#add_item').click(function () {
+    $('UL.my_list').append('<li>Item</li>');
+  });
+  $('DIV#remove_item').click(function () {
+    // $("UL.my_list").remove("li:last-child");
+    $('UL.my_list').children().last().remove();
+  });
+  $('DIV#clear_list').click(function () {
+    $('UL.my_list').children().remove();
+    // $("UL.my_list").empty();
+    // $("UL.my_list").remove();
+    // All three of these work.
+    // NOTE: The last one removes the entire list, not just the children.
+  });
+});
