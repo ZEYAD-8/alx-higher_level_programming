e74b7fb859cfd956c64797f716983f5eb013b20c
ZEYAD-8 <zeyadosama15@gmail.com>
2024-03-04 04:29:08 +0200

    Star wars character

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,9 @@
+// 7. Star Wars character
+
+$.ajax({
+  type: 'GET',
+  url: 'https://swapi-api.alx-tools.com/api/people/5/?format=json',
+  success: function (data) {
+    $('DIV#character').text(data.name);
+  }
+});
